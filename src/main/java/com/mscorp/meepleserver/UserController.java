5b29c1cbb80e9592dbdabc1bd1eb9615688db0a1
Semyon <ssshakurov@edu.hse.ru>
2021-05-07 22:50:49 +0300

    try to fix

--- src/main/java/com/mscorp/meepleserver/UserController.java
+++ src/main/java/com/mscorp/meepleserver/UserController.java
@@ -7,17 +7,12 @@ import com.mscorp.meepleserver.services.FileStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -209,23 +204,8 @@ public class UserController {
     }
 
     @GetMapping("/downloadFile/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        Resource resource = fileStorageService.loadFileAsResource(fileName);
-
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
-        }
-
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+    public
+    @ResponseBody byte[] downloadFile(@PathVariable String fileName) throws IOException {
+        return this.fileStorageService.loadFileAsResource(fileName);
     }
 }
--- src/main/java/com/mscorp/meepleserver/services/FileStorageService.java
+++ src/main/java/com/mscorp/meepleserver/services/FileStorageService.java
@@ -2,16 +2,13 @@ package com.mscorp.meepleserver.services;
 
 import com.mscorp.meepleserver.FileStorageProperties;
 import com.mscorp.meepleserver.exceptions.FileStorageException;
-import com.mscorp.meepleserver.exceptions.MyFileNotFoundException;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -51,17 +48,8 @@ public class FileStorageService {
         }
     }
 
-    public Resource loadFileAsResource(String fileName) {
-        try {
+    public byte[] loadFileAsResource(String fileName) throws IOException {
             Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
-            if(resource.exists()) {
-                return resource;
-            } else {
-                throw new MyFileNotFoundException("File not found " + fileName);
-            }
-        } catch (MalformedURLException ex) {
-            throw new MyFileNotFoundException("File not found " + fileName, ex);
-        }
+            return IOUtils.toByteArray(filePath.toUri());
     }
 }
